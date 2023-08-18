866ec9736015919b5c79bec574500b90f968bd5e
limeunju <enjulim7056@gmail.com>
2023-08-18 14:58:20 +0900

    feat: history를 화면에 표시

--- app/src/main/java/com/example/myapplication/adapter/HistoryAdapter.kt
+++ app/src/main/java/com/example/myapplication/adapter/HistoryAdapter.kt
@@ -11,36 +11,50 @@ import com.starFaceFinder.data.model.SimilarFace
 
 class HistoryAdapter(
     var histories: ArrayList<Pair<FaceInfo, List<SimilarFace>>> = arrayListOf()
-): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    inner class HistoryViewHolder(private val binding: ViewHistoryBinding): RecyclerView.ViewHolder(binding.root){
-        fun bind(history: Pair<FaceInfo, List<SimilarFace>>){
+    inner class HistoryViewHolder(private val binding: ViewHistoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(history: Pair<FaceInfo, List<SimilarFace>>) {
             val faceInfo = history.first
             val similarFaces = history.second
 
-            Glide
-                .with(binding.root.rootView.context)
-                .load(faceInfo.fileUri)
-                .dontAnimate()
-                .override(300,300)
-                .into(binding.profileImg)
+            with(binding) {
+                ageTxt.text = "${faceInfo.age}세"
+                genderTxt.text = when (faceInfo.gender) {
+                    "male" -> "남성"
+                    "female" -> "여성"
+                    "child" -> "어린이"
+                    else -> null
+                }
+                similarTxt.text = similarFaces.firstOrNull()?.name
+
+                Glide
+                    .with(root.rootView.context)
+                    .load(faceInfo.fileUri)
+                    .dontAnimate()
+                    .override(300, 300)
+                    .into(profileImg)
+            }
         }
     }
 
-    inner class EmptyViewHolder(private val binding: ViewHistoryEmptyBinding): RecyclerView.ViewHolder(binding.root)
+    inner class EmptyViewHolder(private val binding: ViewHistoryEmptyBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
         when {
             histories.isNotEmpty() -> {
-               HistoryViewHolder(ViewHistoryBinding.inflate(LayoutInflater.from(parent.context)))
+                HistoryViewHolder(ViewHistoryBinding.inflate(LayoutInflater.from(parent.context)))
             }
+
             else -> EmptyViewHolder(ViewHistoryEmptyBinding.inflate(LayoutInflater.from(parent.context)))
         }
 
     override fun getItemCount(): Int = histories.size
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when(holder){
+        when (holder) {
             is HistoryViewHolder -> {
                 holder.bind(histories[position])
             }

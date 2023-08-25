c30ce1fed45ddc5f87762cb3c8b9937fbbdce38e
limeunju <enjulim7056@gmail.com>
2023-08-25 12:24:59 +0900

    feat: fcm service 생성

--- app/src/main/java/com/example/myapplication/common/MainApplication.kt
+++ app/src/main/java/com/example/myapplication/common/MainApplication.kt
@@ -2,6 +2,12 @@ package com.example.myapplication.common
 
 import android.app.Application
 import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import com.example.myapplication.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
+import com.starFaceFinder.data.common.TAG
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -13,5 +19,19 @@ class MainApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         appContext = applicationContext
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            Log.d(TAG, token)
+            Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+        })
     }
 }
